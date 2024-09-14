1f03d0c6aaa48ebd5b95713a9b69b9566d65a302
dilanasasanka <dilanamathugama@gmail.com>
2024-09-14 16:24:39 +0530

    latest updates

--- src/components/SectionTwo.js
+++ src/components/SectionTwo.js
@@ -3,6 +3,9 @@ import artCult from '../images/artcult-thumbnail.png'
 import genify from '../images/genify-thumbnail.png'
 import metrics from '../images/metrics-thumbnail.png'
 import artCult2 from '../images/artcult-1.png'
+import artCult34 from '../images/artcult-34.png'
+import genifyResponsive from '../images/genify-responsive.png'
+import metricsResponsive from '../images/eco-metrics-responsive.png'
 import aws from '../images/aws.png'
 import snowflake from '../images/snowflake.png'
 import percipio from '../images/percipio.png'
@@ -28,28 +31,29 @@ const SectionTwo = () => {
         <div className='section-two-container-1-card'>
           <div 
             className='card-1'
-            key={'3'}
+            key={'1'}
             onClick={() => handleProjectClick('3')}
           >
-            <img src={artCult2} id='arcult-1' alt='art-cult1' />
+            <img src={genifyResponsive} id='arcult-1' className='artcult' alt='art-cult1'/>
             <img src={genify} id='arcult-2' alt='art-cult2' />
             <h1>Genify</h1>
           </div>
           <div 
             className='card-2'
-            key={'1'}
+            key={'2'}
             onClick={() => handleProjectClick('1')}
           >
-            <img src={artCult2} id='arcult-1' alt='art-cult1' />
+            {/* <img src={artCult2} id='arcult-1' alt='art-cult1' /> */}
+            <img src={artCult34} id='arcult-1' className='artcult' alt='art-cult1'/>
             <img src={artCult} id='arcult-2' alt='art-cult2' />
             <h1>ArtCult</h1>
           </div>
           <div 
             className='card-3'
-            key={'2'}
+            key={'3'}
             onClick={() => handleProjectClick('2')}
           >
-            <img src={artCult2} id='arcult-1' alt='art-cult1' />
+            <img src={metricsResponsive} className='metrcis' id='arcult-1' alt='art-cult1'/>
             <img src={metrics} id='arcult-2' alt='art-cult2' />
             <h1>Metrics</h1>
           </div>
